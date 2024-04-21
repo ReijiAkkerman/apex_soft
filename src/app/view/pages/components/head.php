db31e886f2142cd6063c1fe04ee4a41d9db8048a
Nacho2Macho <dmitriy-zverev-2004@mail.ru>
2024-04-21 23:37:24 +0300

    1

---
+++ src/app/view/pages/components/head.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900" rel="stylesheet" />
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/fonts.css">
+<link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/variables.css">
---
+++ src/app/view/pages/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <?php require_once __DIR__ . '/components/head.php' ?>
+    <title>Апекс - Софт</title>
+</head>
+
+<body>
+    <?php require_once __DIR__ . '/components/header.php' ?>
+    <main></main>
+    <footer></footer>
+</body>
+
+</html>
