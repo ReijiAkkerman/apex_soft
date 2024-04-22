d1250be9a53f338785af2e7f7e600f76b625a53e
Nacho2Macho <dmitriy-zverev-2004@mail.ru>
2024-04-22 23:26:29 +0300

    Первоначальная настройка бэкенда

--- src/index.php
+++ src/index.php
@@ -1,2 +1,15 @@
 <?php
-    require_once __DIR__ . '/app/view/pages/main.php';
+    namespace project;
+    
+    use project\core\Router;
+    
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    
+    require_once __DIR__ . '/../vendor/autoload.php';
+    
+    $obj = new Router();
+    $obj->action();
+    
+    // include __DIR__ . '/app/view/pages/main.php';
