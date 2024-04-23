198e0b673864702434b8a9c9b2ee4959fb550d3c
Nacho2Macho <dmitriy-zverev-2004@mail.ru>
2024-04-24 01:21:34 +0300

    Сделать базовую версию header
    Верстка top-header, начал делать выпадающие меню, соглосовать дизайн с
    Колей

--- src/app/view/pages/components/head.php
+++ src/app/view/pages/components/head.php
@@ -1,5 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script defer src="/src/js/Jquery.js"></script>
+<script defer src="/src/js/header.js"></script>
 <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900"
     rel="stylesheet" />
 <link rel="stylesheet" href="/src/css/normalize.css">
--- src/app/view/pages/components/header.php
+++ src/app/view/pages/components/header.php
@@ -2,22 +2,25 @@
     <div class="header__top">
         <div class="container header__container header__container--top">
             <div class="header__left">
-                <p class="header__left-text">ООО «Апекс-софт» <br> официальный представитель 1C в Брянске</p>
+                <p class="header__left-text">ООО «Апекс-софт» официальный представитель 1C в Брянске</p>
             </div>
             <nav class="nav header__nav header__nav--top ">
                 <ul class="nav__list nav__list--top">
-                    <li class="nav__item"><a
+                    <li class="nav__item nav__item--top"><a
                             href="https://yandex.ru/maps/org/apeks_soft/1023193342/?indoorLevel=1&ll=34.344219%2C53.243368&z=17.05"
                             class="nav__link icon-adress">Россия, г. Брянск, переулок Осоавиахима, 3А , офис 603</a>
                     </li>
-                    <li class="nav__item"><a href="tel:+74832320117" class="nav__link icon-phone">+7 (4832) 320-117</a>
+                    <li class="nav__item nav__item--top"><a href="tel:+74832320117" class="nav__link icon-phone">+7
+                            (4832) 320-117</a>
                     </li>
-                    <li class="nav__item"><a href="mailto:info@1capexsoft.ru"
+                    <li class="nav__item nav__item--top"><a href="mailto:info@1capexsoft.ru"
                             class="nav__link icon-mail">info@1capexsoft.ru</a>
                     </li>
-                    <li class="nav__item"><a href="#" class="nav__link icon-calendar-alt-fill">Время работы <time
-                                datetime="10:00">10:00</time>-<time datetime="17:00">17:00</time></a></li>
-                    <li class="nav__item"><a href="#" class="nav__link icon-user">Войти</a></li>
+                    <li class="nav__item nav__item--top"><a href="#" class="nav__link icon-calendar-alt-fill">
+                            <p class="nav__link-text">Время работы <span><time datetime="10:00">10:00</time>-<time
+                                        datetime="17:00">17:00</time></span> </p>
+                        </a></li>
+                    <li class="nav__item nav__item--top"><a href="#" class="nav__link icon-user">Войти</a></li>
                 </ul>
             </nav>
         </div>
@@ -26,94 +29,94 @@
         <div class="container header__container header__container--fixed">
             <a href="/" class="logo header__logo" aria-label="Логотип Апекс Софт"><img src="/src/assets/img/logo.png"
                     alt="Логотип компании «Апекс-софт»"></a>
-            <div class="header__right">
-                <nav class="nav header__nav header__nav--fixed">
-                    <ul class="nav__list nav__list--fixed">
-                        <li class="nav__item nav__item--fixed">
-                            <a href="" class="nav__link nav__link--fixed">Решения</a>
-                            <ul class="nav__list-description description">
-                                <li class="description-list">
-                                    <p class="description-list__title">Базовая</p>
-                                    <ul class="description-list__items">
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1C: Бухгалтерия 8. Базовая
-                                                Версия</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Зарплата и Управление
-                                                Персоналом 8. Базовая версия</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Управление торговлей 8.
-                                                Базовая версия</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Розница 8. Базовая версия.</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Управление нашей фирмой 8.
-                                                Базовая версия</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="description-list">
-                                    <p class="description-list__title">ПРОФ</p>
-                                    <ul class="description-list__items">
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Бухгалтерия 8</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Зарплата и Управление
-                                                Персоналом 8</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Управление торговлей 8</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Розница 8</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Управление нашей фирмой 8</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Комплексная автоматизация
-                                                8</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="description-list">
-                                    <p class="description-list__title">БЮДЖЕТ</p>
-                                    <ul class="description-list__items">
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Зарплата и кадры
-                                                государственного учреждения 8</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Бухгалтерия государственного
-                                                учреждения 8</a>
-                                        </li>
-                                        <li class="description-list__item">
-                                            <a href="#" class="description-list__link">1С:Бухгалтерия государственного
-                                                учреждения 8. Базовая версия</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="nav__item nav__item--fixed">
-                            <a href="" class="nav__link nav__link--fixed">Каталог</a>
-                            <ul class="nav__list-description"></ul>
-                        </li>
-                        <li class="nav__item nav__item--fixed">
-                            <a href="" class="nav__link nav__link--fixed">Каталог</a>
-                        </li>
-                        <li class="nav__item nav__item--fixed">
-                            <a href="" class="nav__link nav__link--fixed">Информация</a>
-                            <ul class="nav__list-description"></ul>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            <nav class="nav header__nav header__nav--fixed">
+                <ul class="nav__list nav__list--fixed">
+                    <li class="nav__item nav__item--fixed">
+                        <a href="" class="nav__link nav__link--fixed">Решения</a>
+                        <ul class="nav__list-description description">
+                            <li class="description-list">
+                                <p class="description-list__title">Базовая</p>
+                                <ul class="description-list__items">
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1C: Бухгалтерия 8. Базовая
+                                            Версия</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Зарплата и Управление
+                                            Персоналом 8. Базовая версия</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Управление торговлей 8.
+                                            Базовая версия</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Розница 8. Базовая версия.</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Управление нашей фирмой 8.
+                                            Базовая версия</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="description-list">
+                                <p class="description-list__title">ПРОФ</p>
+                                <ul class="description-list__items">
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Бухгалтерия 8</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Зарплата и Управление
+                                            Персоналом 8</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Управление торговлей 8</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Розница 8</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Управление нашей фирмой 8</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Комплексная автоматизация
+                                            8</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="description-list">
+                                <p class="description-list__title">БЮДЖЕТ</p>
+                                <ul class="description-list__items">
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Зарплата и кадры
+                                            государственного учреждения 8</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Бухгалтерия государственного
+                                            учреждения 8</a>
+                                    </li>
+                                    <li class="description-list__item">
+                                        <a href="#" class="description-list__link">1С:Бухгалтерия государственного
+                                            учреждения 8. Базовая версия</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav__item nav__item--fixed">
+                        <a href="" class="nav__link nav__link--fixed">Каталог</a>
+                        <ul class="nav__list-description description"></ul>
+                    </li>
+                    <li class="nav__item nav__item--fixed">
+                        <a href="" class="nav__link nav__link--fixed">Контакы</a>
+                    </li>
+                    <li class="nav__item nav__item--fixed">
+                        <a href="" class="nav__link nav__link--fixed">Информация</a>
+                        <ul class="nav__list-description description"></ul>
+                    </li>
+                </ul>
+            </nav>
+            <!-- <div class="header__right">
+            </div> -->
         </div>
     </div>
 </header>
