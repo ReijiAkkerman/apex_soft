3acc72846ef690857d0dd2433fc15cdf42f8d8b5
ReijiAkkerman <reijiakkerman@gmail.com>
2024-05-27 02:04:42 +0300

    1

--- src/js/Reiji/async/Order.js
+++ src/js/Reiji/async/Order.js
@@ -58,12 +58,18 @@ class Order {
             if(xhr.response !== null && xhr.response.hasOwnProperty('error_message')) {
                 if(xhr.response['error_message']) {
                     if(xhr.response['error_message'].includes('|')) {
+                        let preloader = document.querySelector('.preloader');
+                        preloader.style.zIndex = '-1';
+                        preloader.style.opacity = '0';
                         let temp_array = xhr.response['error_message'].split('|');
                         let element = document.querySelector('#' + temp_array[0]);
                         element.textContent = temp_array[1];
                         element.style.display = 'block';
                     }
                     else {
+                        let preloader = document.querySelector('.preloader');
+                        preloader.style.zIndex = '-1';
+                        preloader.style.opacity = '0';
                         alert(xhr.response['error_message']);
                     }
                 }
