03258efa529c5f0328eadb8b9a1d2ba85ca9b9e2
ReijiAkkerman <reijiakkerman@gmail.com>
2024-06-07 00:39:30 +0300

    12

--- src/js/Reiji/async/Auth.js
+++ src/js/Reiji/async/Auth.js
@@ -188,6 +188,187 @@ class Auth {
         };
     }
 
+    registrateMobile(event) {
+        event.preventDefault();
+        let currentPagename = (function() {
+            let path = window.location.pathname;
+            let pagenames_array = path.split('/');
+            let pagename = pagenames_array[1];
+            return pagename;
+        })();
+        let data = new FormData(document.querySelector('.Reiji_registration1'));
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', `/${currentPagename}/registrate`);
+        xhr.send(data);
+        xhr.responseType = 'json';
+        xhr.onload = () => {
+            // alert(xhr.response);
+            if(xhr.response.hasOwnProperty('error_message')) {
+                if(xhr.response['error_message']) {
+                    let result = xhr.response['error_message'].indexOf('|');
+                    if(result < 0) 
+                        alert(xhr.response['error_message']);
+                    else {
+                        let str_array = xhr.response['error_message'].split('|');
+                        let field = str_array[0];
+                        let message = str_array[1];
+                        let error_text = document.querySelector(`.Reiji_registration1 p._${field}`);
+                        let error_field = document.querySelector(`.Reiji_registration1 input[name="${field}"]`);
+                        error_text.textContent = message;
+                        error_field.classList.add('form__error-field');
+                    }
+                }
+                else {
+                    Auth.viewParseError(xhr.response);
+                }
+            }
+            else {
+                if(xhr.response.hasOwnProperty('name')) {
+                    if(xhr.response['name']) {
+                        let element = document.querySelector('#username');
+                        element.textContent = xhr.response['name'];
+                        AuthPopup.showExitButton();
+                        AuthPopup.showCartButton();
+                        let admin = Auth.getCookie('is_admin');
+                        if(admin) {
+                            if((window.location.pathname.split('/')[1]) == 'product')
+                                AdminForm.showAdminFunctions();
+                            if((window.location.pathname.split('/')[1]) == 'catalog')
+                                AdminForm.showAddButton();
+                        }
+                        let array = [
+                            '.header__cabinet-form--mobile',
+                            '.header__cabinet-wrapper',
+                            'body'
+                        ];
+                        for(let i = 0; i < array.length; i++) {
+                            let element = document.querySelector(array[i]);
+                            element.classList.remove('active');
+                        }
+                        Cart.showCartButtons();
+                        let login = document.querySelector('.Reiji_showAuthForm-button');
+                        login.removeEventListener('click', Header.openFormMobile);
+                        let orders = document.querySelector('#order_story');
+                        orders.style.display = 'block';
+                        if((window.location.pathname.split('/')[1]) == 'catalog') {
+                            let elements = document.querySelectorAll('.Reiji_delete-button');
+                            for(let i = 0; i < elements.length; i++) {
+                                if(Auth.getCookie('is_admin')) {
+                                    elements[i].removeAttribute('style');
+                                    elements[i].classList.add('product-card__checkmark');
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        Auth.viewParseError(xhr.response);
+                    }
+                }
+                else {
+                    Auth.viewParseError(xhr.response);
+                }
+            }
+        };
+    }
+
+    loginMobile(event) {
+        event.preventDefault();
+        let currentPagename = (function() {
+            let path = window.location.pathname;
+            let pagenames_array = path.split('/');
+            let pagename = pagenames_array[1];
+            return pagename;
+        })();
+        let data = new FormData(document.querySelector('.Reiji_login1'));
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', `/${currentPagename}/login`);
+        xhr.send(data);
+        xhr.responseType = 'json';
+        xhr.onload = () => {
+            // alert(xhr.response);
+            if(xhr.response.hasOwnProperty('error_message')) {
+                if(xhr.response['error_message']) {
+                    let result = xhr.response['error_message'].indexOf('|');
+                    if(result < 0) 
+                        alert(xhr.response['error_message']);
+                    else {
+                        let result = xhr.response['error_message'].indexOf('&');
+                        if(result < 0) {
+                            let str_array = xhr.response['error_message'].split('|');
+                            let field = str_array[0];
+                            let message = str_array[1];
+                            let error_text = document.querySelector(`.Reiji_login1 p._${field}`);
+                            let error_field = document.querySelector(`.Reiji_login1 input[name="${field}"]`);
+                            error_text.textContent = message;
+                            error_field.classList.add('form__error-field');
+                        }
+                        else {
+                            let str_array = xhr.response['error_message'].split('|');
+                            let labels_array = str_array[0].split('&');
+                            let message = str_array[1];
+                            let error_text = document.querySelector('.Reiji_login1 p._login');
+                            let error_field;
+                            error_text.textContent = message;
+                            for(let i = 0; i < labels_array.length; i++) {
+                                error_field = document.querySelector(`.Reiji_login1 input[name="${labels_array[i]}"]`);
+                                error_field.classList.add('form__error-field');
+                            }
+                        }
+                    }
+                }
+                else {
+                    Auth.viewParseError(xhr.response);
+                }
+            }
+            else {
+                if(xhr.response.hasOwnProperty('name')) {
+                    if(xhr.response['name']) {
+                        let element = document.querySelector('#username');
+                        element.textContent = xhr.response['name'];
+                        AuthPopup.showExitButton();
+                        AuthPopup.showCartButton();
+                        let admin = Auth.getCookie('is_admin');
+                        if(admin) {
+                            if((window.location.pathname.split('/')[1]) == 'product')
+                                AdminForm.showAdminFunctions();
+                            if((window.location.pathname.split('/')[1]) == 'catalog')
+                                AdminForm.showAddButton();
+                        }
+                        let array = [
+                            '.header__cabinet-form--mobile',
+                            '.header__cabinet-wrapper',
+                            'body'
+                        ];
+                        for(let i = 0; i < array.length; i++) {
+                            let element = document.querySelector(array[i]);
+                            element.classList.remove('active');
+                        }
+                        Cart.showCartButtons();
+                        let login = document.querySelector('.Reiji_showAuthForm-button');
+                        login.removeEventListener('click', Header.openFormMobile);
+                        let orders = document.querySelector('#order_story');
+                        orders.style.display = 'block';
+                        if((window.location.pathname.split('/')[1]) == 'catalog') {
+                            let elements = document.querySelectorAll('.Reiji_delete-button');
+                            for(let i = 0; i < elements.length; i++) {
+                                if(Auth.getCookie('is_admin')) {
+                                    elements[i].removeAttribute('style');
+                                    elements[i].classList.add('product-card__checkmark');
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        Auth.viewParseError(xhr.response);
+                    }
+                }
+                else {
+                    Auth.viewParseError(xhr.response);
+                }
+            }
+        };
+    }
+
     static exit() {
         let currentPagename = (function() {
             let path = window.location.pathname;
@@ -209,6 +390,8 @@ class Auth {
             Cart.hideCartButtons();
             let login = document.querySelector('.Reiji_showAuthForm-button');
             login.addEventListener('click', Header.openForm);
+            login = document.querySelector('.Reiji_showAuthForm-button');
+            login.addEventListener('click', Header.openFormMobile);
             let orders = document.querySelector('#order_story');
             orders.style.display = 'none';
             if((window.location.pathname.split('/')[1]) == 'catalog') {
@@ -266,6 +449,10 @@ document.addEventListener('DOMContentLoaded', function() {
     element.addEventListener('click', auth.registrate);
     element = document.querySelector('#login_button');
     element.addEventListener('click', auth.login);
+    element = document.querySelector('#registration_button1');
+    element.addEventListener('click', auth.registrateMobile);
+    element = document.querySelector('#login_button1');
+    element.addEventListener('click', auth.loginMobile);
     let elements;
     let elements_array = [
         'login',
